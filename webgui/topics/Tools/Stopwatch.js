166de9c7e39c4762c4996d6be978dfd4620ef590
Torsten Dreyer <torsten@3r.de>
2015-03-17 10:41:04 +0100

    Phi: Detachable Stopwatch

--- webgui/topics/Tools/Stopwatch.js
+++ webgui/topics/Tools/Stopwatch.js
@@ -1,5 +1,5 @@
 define([
-        'jquery', 'knockout', 'text!./Stopwatch.html', 'kojqui/button'
+        'jquery', 'knockout', 'text!./Stopwatch.html', 'jquery-ui/dialog', 'kojqui/button'
 ], function(jquery, ko, htmlString) {
 
     function ViewModel(params) {
@@ -10,6 +10,17 @@ define([
         self.addWatch = function() {
             self.watches.push(self.watches().length);
         }
+        
+        self.toDialog = function(a,evt) {
+            var p = jquery(evt.target).parent();
+            p.next().dialog({ 
+                title: p.text(),
+                closeOnEscape: false,
+                });
+            p.remove();
+        }
+        
+        self.addWatch();
     }
 
      ViewModel.prototype.dispose = function() {

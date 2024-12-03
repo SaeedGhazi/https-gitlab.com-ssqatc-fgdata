39863b98f1b1ae9d76f91c41a1e7140f38dabed6
Fernando García Liñán <fernando@fgarlin.com>
2024-12-03 12:33:38 +0100

    Better noise functions
    Use an integer hash instead of the infamous sin-based one-liner. This
    should offer good performance and much higher quality noise, specially
    at high magnitudes.

--- Shaders/HDR/night_vision.glsl
+++ Shaders/HDR/night_vision.glsl
@@ -6,14 +6,14 @@ uniform vec4 fg_Viewport;
 uniform float osg_SimulationTime;
 
 const float FLICKER_AMOUNT = 0.05;
-const float NOISE_AMOUNT = 0.03;
-const float GRAIN_SIZE = 3.0;
+const float NOISE_AMOUNT = 0.05;
+const float GRAIN_SIZE = 1.2;
 
 // color.glsl
 float linear_srgb_to_luminance(vec3 color);
 // noise.glsl
 float rand_1d(float n);
-float snoise_3d(vec3 v);
+float noise_3d(vec3 x);
 
 vec3 night_vision_apply(vec3 color, vec2 uv)
 {
@@ -21,8 +21,9 @@ vec3 night_vision_apply(vec3 color, vec2 uv)
         return color;
     }
     float lum = linear_srgb_to_luminance(color);
-    float noise = snoise_3d(vec3(uv * fg_Viewport.zw / GRAIN_SIZE, osg_SimulationTime * 10.0));
-    lum += noise * NOISE_AMOUNT;
+    float noise = noise_3d(vec3(uv * fg_Viewport.zw / GRAIN_SIZE,
+                                mod(osg_SimulationTime * 10000.0, 10000.0)));
+    lum += (noise - 0.5) * NOISE_AMOUNT;
     float flicker = rand_1d(osg_SimulationTime) * FLICKER_AMOUNT;
     vec3 green_factor = vec3(0.1, 1.0 - FLICKER_AMOUNT + flicker, 0.2);
     color = lum * green_factor;
--- Shaders/HDR/noise.glsl
+++ Shaders/HDR/noise.glsl
@@ -1,116 +1,151 @@
 $FG_GLSL_VERSION
-/*
- * This is a library of noise functions, taking a coordinate vector and
- * a wavelength as input and returning a number [0:1] as output.
 
- * - Noise2D() is 2d Perlin noise.
- * - Noise3D() is 3d Perlin noise.
- * - DotNoise2D() is sparse dot noise and takes a dot density parameter.
- * - DropletNoise2D() is sparse dot noise modified to look like liquid and
- *   takes a dot density parameter.
- * - VoronoiNoise2D() is a function mapping the terrain into random domains,
- *   based on Voronoi tiling of a regular grid distorted with xrand and yrand.
- * - SlopeLines2D() computes a semi-random set of lines along the direction of
- *   steepest descent, allowing to simulate e.g. water erosion patterns.
- * - Strata3D() computes a vertically stratified random pattern, appropriate
- *   e.g. for rock textures
- *
- * Thorsten Renk 2014
- */
+#define NOISE_SMOOTH_INTERP_METHOD 0
 
-float rand_1d(float n)
+// We define pi locally here to avoid including math.glsl
+// noise.glsl should be independent.
+const float NOISE_PI = 3.14159265358979323846;
+
+// Based on the best integer hash by Chris Wellons and TheIronBorn
+// https://www.shadertoy.com/view/dllSW7
+// https://github.com/skeeto/hash-prospector/issues/23
+uint hash(uint x)
 {
-    return fract(sin(n) * 43758.5453123);
+    x ^= x >> 15;
+    x = (x | 1u) ^ (x * x);
+    x ^= x >> 17;
+    x *= 0x9E3779B9u;
+    x ^= x >> 13;
+    return x;
 }
 
-float rand_2d(vec2 co)
-{
-    return fract(sin(dot(co.xy, vec2(12.9898,78.233))) * 43758.5453);
+// Compound versions
+uint hash(uvec2 x) {
+    return hash(x.x ^ hash(x.y));
+}
+uint hash(uvec3 x) {
+    return hash(x.x ^ hash(x.y) ^ hash(x.z));
+}
+uint hash(uvec4 x) {
+    return hash(x.x ^ hash(x.y) ^ hash(x.z) ^ hash(x.w));
 }
 
-float rand_3d(vec3 co)
-{
-    return fract(sin(dot(co.xyz, vec3(12.9898,78.233,144.7272))) * 43758.5453);
+float uint_to_normalized_float(uint x) {
+    return uintBitsToFloat(x & 0x007FFFFFu | 0x3F800000u) - 1.0;
 }
 
-float cosine_interpolate(float a, float b, float x)
-{
-    float ft = x * 3.1415927;
-    float f = (1.0 - cos(ft)) * 0.5;
-    return a * (1.0 - f) + b * f;
+// Random value using unsigned integer seeds
+float rand_1d(uint x) {
+    return uint_to_normalized_float(hash(x));
+}
+float rand_2d(uvec2 x) {
+    return uint_to_normalized_float(hash(x));
+}
+float rand_3d(uvec3 x) {
+    return uint_to_normalized_float(hash(x));
+}
+float rand_4d(uvec4 x) {
+    return uint_to_normalized_float(hash(x));
 }
 
-float simple_interpolate(float a, float b, float x)
-{
-    return a + smoothstep(0.0, 1.0, x) * (b - a);
+// Random value using floating point seeds
+float rand_1d(float x) {
+    return uint_to_normalized_float(hash(floatBitsToUint(x)));
+}
+float rand_2d(vec2 x) {
+    return uint_to_normalized_float(hash(floatBitsToUint(x)));
+}
+float rand_3d(vec3 x) {
+    return uint_to_normalized_float(hash(floatBitsToUint(x)));
+}
+float rand_4d(vec4 x) {
+    return uint_to_normalized_float(hash(floatBitsToUint(x)));
 }
 
-float interpolated_noise_2d(vec2 coord)
+
+vec2 smooth_interp(vec2 a)
 {
-    float x = coord.x;
-    float y = coord.y;
-
-    float integer_x    = x - fract(x);
-    float fractional_x = x - integer_x;
-
-    float integer_y    = y - fract(y);
-    float fractional_y = y - integer_y;
-
-    float v1 = rand_2d(vec2(integer_x,       integer_y));
-    float v2 = rand_2d(vec2(integer_x + 1.0, integer_y));
-    float v3 = rand_2d(vec2(integer_x,       integer_y + 1.0));
-    float v4 = rand_2d(vec2(integer_x + 1.0, integer_y + 1.0));
-
-    float i1 = simple_interpolate(v1, v2, fractional_x);
-    float i2 = simple_interpolate(v3, v4, fractional_x);
-
-    return simple_interpolate(i1, i2, fractional_y);
+#if NOISE_SMOOTH_INTERP_METHOD == 0 // Cubic Hermite
+    return smoothstep(0.0, 1.0, a);
+#elif NOISE_SMOOTH_INTERP_METHOD == 1 // Quintic Hermite
+    return a * a * a * (a * (a * 6.0 - 15.0) + 10.0);
+#elif NOISE_SMOOTH_INTERP_METHOD == 2 // Cosine
+    return (1.0 - cos(a * NOISE_PI)) * 0.5;
+#else // Linear
+    return a;
+#endif
 }
 
-float interpolated_noise_3d(vec3 coord)
+vec3 smooth_interp(vec3 a)
 {
-    float x = coord.x;
-    float y = coord.y;
-    float z = coord.z;
+#if NOISE_SMOOTH_INTERP_METHOD == 0 // Cubic Hermite
+    return smoothstep(0.0, 1.0, a);
+#elif NOISE_SMOOTH_INTERP_METHOD == 1 // Quintic Hermite
+    return a * a * a * (a * (a * 6.0 - 15.0) + 10.0);
+#elif NOISE_SMOOTH_INTERP_METHOD == 2 // Cosine
+    return (1.0 - cos(a * NOISE_PI)) * 0.5;
+#else // Linear
+    return a;
+#endif
+}
 
-    float integer_x    = x - fract(x);
-    float fractional_x = x - integer_x;
+float bilinear_interp(float p00, float p01, float p10, float p11, vec2 u)
+{
+    vec2 px = mix(vec2(p00, p01), vec2(p10, p11), u.x);
+    return mix(px.x, px.y, u.y);
+}
 
-    float integer_y    = y - fract(y);
-    float fractional_y = y - integer_y;
+float trilinear_interp(float p000, float p001, float p010, float p011,
+                       float p100, float p101, float p110, float p111,
+                       vec3 u)
+{
+    vec4 pz = mix(vec4(p000, p010, p100, p110),
+                  vec4(p001, p011, p101, p111),
+                  u.z);
+    vec2 px = mix(pz.xy, pz.zw, u.x);
+    return mix(px.x, px.y, u.y);
+}
 
-    float integer_z    = z - fract(z);
-    float fractional_z = z - integer_z;
+// 2D value noise
+float noise_2d(vec2 x)
+{
+    vec2 i = floor(x);
+    vec2 f = fract(x);
+    vec2 u = smooth_interp(f);
+    float g00 = rand_2d(i + vec2(0.0, 0.0));
+    float g10 = rand_2d(i + vec2(1.0, 0.0));
+    float g01 = rand_2d(i + vec2(0.0, 1.0));
+    float g11 = rand_2d(i + vec2(1.0, 1.0));
+    return bilinear_interp(g00, g01, g10, g11, u);
+}
 
-    float v1 = rand_3d(vec3(integer_x,       integer_y,       integer_z));
-    float v2 = rand_3d(vec3(integer_x + 1.0, integer_y,       integer_z));
-    float v3 = rand_3d(vec3(integer_x,       integer_y + 1.0, integer_z));
-    float v4 = rand_3d(vec3(integer_x + 1.0, integer_y + 1.0, integer_z));
-
-    float v5 = rand_3d(vec3(integer_x,       integer_y,       integer_z + 1.0));
-    float v6 = rand_3d(vec3(integer_x + 1.0, integer_y,       integer_z + 1.0));
-    float v7 = rand_3d(vec3(integer_x,       integer_y + 1.0, integer_z + 1.0));
-    float v8 = rand_3d(vec3(integer_x + 1.0, integer_y + 1.0, integer_z + 1.0));
-
-    float i1 = simple_interpolate(v1, v5, fractional_z);
-    float i2 = simple_interpolate(v2, v6, fractional_z);
-    float i3 = simple_interpolate(v3, v7, fractional_z);
-    float i4 = simple_interpolate(v4, v8, fractional_z);
-
-    float ii1 = simple_interpolate(i1, i2, fractional_x);
-    float ii2 = simple_interpolate(i3, i4, fractional_x);
-
-    return simple_interpolate(ii1, ii2, fractional_y);
+// 3D value noise
+float noise_3d(vec3 x)
+{
+    vec3 i = floor(x);
+    vec3 f = fract(x);
+    vec3 u = smooth_interp(f);
+    float g000 = rand_3d(i + vec3(0.0, 0.0, 0.0));
+    float g100 = rand_3d(i + vec3(1.0, 0.0, 0.0));
+    float g010 = rand_3d(i + vec3(0.0, 1.0, 0.0));
+    float g110 = rand_3d(i + vec3(1.0, 1.0, 0.0));
+    float g001 = rand_3d(i + vec3(0.0, 0.0, 1.0));
+    float g101 = rand_3d(i + vec3(1.0, 0.0, 1.0));
+    float g011 = rand_3d(i + vec3(0.0, 1.0, 1.0));
+    float g111 = rand_3d(i + vec3(1.0, 1.0, 1.0));
+    return trilinear_interp(g000, g001, g010, g011,
+                            g100, g101, g110, g111,
+                            u);
 }
 
 float noise_2d(vec2 coord, float wavelength)
 {
-    return interpolated_noise_2d(coord / wavelength);
+    return noise_2d(coord / wavelength);
 }
 
 float noise_3d(vec3 coord, float wavelength)
 {
-    return interpolated_noise_3d(coord / wavelength);
+    return noise_3d(coord / wavelength);
 }
 
 float dot_noise_2d(vec2 coord, float fractional_max_dot_size, float d_density)
@@ -227,102 +262,4 @@ float slope_lines_2d(vec2 coord, vec2 s, float steepness)
 float slope_lines_2d(vec2 coord, vec2 grad_dir, float wavelength, float steepness)
 {
     return slope_lines_2d(coord / wavelength, grad_dir, steepness);
-}
-
-/*
- * 3D Simplex noise by Ian McEwan, Ashima Arts.
- * Copyright (C) 2011 Ashima Arts. All rights reserved.
- * Distributed under the MIT License.
- * https://github.com/ashima/webgl-noise
- */
-
-vec3 mod289(vec3 x) {
-    return x - floor(x * (1.0 / 289.0)) * 289.0;
-}
-
-vec4 mod289(vec4 x) {
-    return x - floor(x * (1.0 / 289.0)) * 289.0;
-}
-
-vec4 permute(vec4 x) {
-    return mod289(((x*34.0)+10.0)*x);
-}
-
-vec4 taylor_inv_sqrt(vec4 r) {
-    return 1.79284291400159 - 0.85373472095314 * r;
-}
-
-float snoise_3d(vec3 v)
-{
-    const vec2 C = vec2(1.0/6.0, 1.0/3.0);
-    const vec4 D = vec4(0.0, 0.5, 1.0, 2.0);
-
-    // First corner
-    vec3 i  = floor(v + dot(v, C.yyy) );
-    vec3 x0 =   v - i + dot(i, C.xxx) ;
-
-    // Other corners
-    vec3 g = step(x0.yzx, x0.xyz);
-    vec3 l = 1.0 - g;
-    vec3 i1 = min( g.xyz, l.zxy );
-    vec3 i2 = max( g.xyz, l.zxy );
-
-    //   x0 = x0 - 0.0 + 0.0 * C.xxx;
-    //   x1 = x0 - i1  + 1.0 * C.xxx;
-    //   x2 = x0 - i2  + 2.0 * C.xxx;
-    //   x3 = x0 - 1.0 + 3.0 * C.xxx;
-    vec3 x1 = x0 - i1 + C.xxx;
-    vec3 x2 = x0 - i2 + C.yyy; // 2.0*C.x = 1/3 = C.y
-    vec3 x3 = x0 - D.yyy;      // -1.0+3.0*C.x = -0.5 = -D.y
-
-    // Permutations
-    i = mod289(i);
-    vec4 p = permute( permute( permute(
-                                   i.z + vec4(0.0, i1.z, i2.z, 1.0 ))
-                               + i.y + vec4(0.0, i1.y, i2.y, 1.0 ))
-                      + i.x + vec4(0.0, i1.x, i2.x, 1.0 ));
-
-    // Gradients: 7x7 points over a square, mapped onto an octahedron.
-    // The ring size 17*17 = 289 is close to a multiple of 49 (49*6 = 294)
-    float n_ = 0.142857142857; // 1.0/7.0
-    vec3  ns = n_ * D.wyz - D.xzx;
-
-    vec4 j = p - 49.0 * floor(p * ns.z * ns.z);  //  mod(p,7*7)
-
-    vec4 x_ = floor(j * ns.z);
-    vec4 y_ = floor(j - 7.0 * x_ );    // mod(j,N)
-
-    vec4 x = x_ *ns.x + ns.yyyy;
-    vec4 y = y_ *ns.x + ns.yyyy;
-    vec4 h = 1.0 - abs(x) - abs(y);
-
-    vec4 b0 = vec4( x.xy, y.xy );
-    vec4 b1 = vec4( x.zw, y.zw );
-
-    //vec4 s0 = vec4(lessThan(b0,0.0))*2.0 - 1.0;
-    //vec4 s1 = vec4(lessThan(b1,0.0))*2.0 - 1.0;
-    vec4 s0 = floor(b0)*2.0 + 1.0;
-    vec4 s1 = floor(b1)*2.0 + 1.0;
-    vec4 sh = -step(h, vec4(0.0));
-
-    vec4 a0 = b0.xzyw + s0.xzyw*sh.xxyy ;
-    vec4 a1 = b1.xzyw + s1.xzyw*sh.zzww ;
-
-    vec3 p0 = vec3(a0.xy,h.x);
-    vec3 p1 = vec3(a0.zw,h.y);
-    vec3 p2 = vec3(a1.xy,h.z);
-    vec3 p3 = vec3(a1.zw,h.w);
-
-    //Normalise gradients
-    vec4 norm = taylor_inv_sqrt(vec4(dot(p0,p0), dot(p1,p1), dot(p2, p2), dot(p3,p3)));
-    p0 *= norm.x;
-    p1 *= norm.y;
-    p2 *= norm.z;
-    p3 *= norm.w;
-
-    // Mix final noise value
-    vec4 m = max(0.5 - vec4(dot(x0,x0), dot(x1,x1), dot(x2,x2), dot(x3,x3)), 0.0);
-    m = m * m;
-    return 105.0 * dot( m*m, vec4( dot(p0,x0), dot(p1,x1),
-                                   dot(p2,x2), dot(p3,x3) ) );
 }

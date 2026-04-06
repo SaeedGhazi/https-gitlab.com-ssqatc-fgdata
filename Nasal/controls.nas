SaeedGhazi/https-gitlab.com-ssqatc-fgdata

commit a4456cc65f0658e8d57c8b3e79fc136cdde65365

VR: Enable 360 mouse mode
Connect up a 360 mouse mode, unlinking the 3D mouse cursor from the OS
mouse cursor (which is constrained and hidden), allowing it to be moved
around freely in the scene, even out of the current view, without the OS
mouse cursor accidentally leaving the window, which is useful for VR
where the user can't even see the OS window.

This mode can be toggled explicitly when VR is enabled (or if the VR
mouse cursor is always shown) with Ctrl+Tab, or with Shift+Right-Click.
If user presence is supported by the OpenXR runtime, 360 mouse mode can
also be enabled/disabled automatically when the VR headset is put on or
removed. This can be controlled with a new option in the VR options
dialog: "Enable 360 mouse when VR in use".

Nasal/controls.nas
@@ -772,13 +772,21 @@ var cycleMouseMode = func(node)
     }
 
     var modeNode = props.globals.getNode('/devices/status/mice/mouse[0]/mode');
-    var mode = modeNode.getValue() + 1;
+    var mode = modeNode.getValue();
+    # 360 mouse mode: treat mode 4 (360) as mode 0 for cycling
+    if (mode == 4) mode = 0;
+    mode += 1;
 
     if ((mode == 1) and getprop('/sim/mouse/skip-flight-controls-mode')) {
         mode +=1;
     }
 
     if (mode == 3) mode = 0;
+
+    # 360 mouse mode: treat mode 0 as mode 4 (360)
+    var cur360 = props.globals.getNode('/sim/vr/cursors/mouse[0]/vr-mode').getValue();
+    if (cur360 and mode == 0) mode = 4;
+
     modeNode.setIntValue(mode);
 
     # this is really a 'show on-screen hints' control
@@ -804,8 +812,14 @@ var cycleMouseMode = func(node)
 
     if (mode == 1) {
         msg = "Mouse is controlling flight controls.";
-    } else {
+    } else if (mode == 2) {
         msg = "Mouse is controlling view direction.";
+    } else { # mode == 4
+        msg = "Mouse is in 360 mode.";
+        suffix = "Press Ctrl + TAB to turn off.";
+        if (reason == "right-click") {
+            suffix = "Press Shift + Right-click to turn off.";
+        }
     }
     
     msg = msg ~ " " ~ suffix;
@@ -814,6 +828,90 @@ var cycleMouseMode = func(node)
 }
 
 addcommand("cycle-mouse-mode", cycleMouseMode);
+
+var toggle360Mouse = func(node)
+{
+    var reason = node.getChild("reason").getValue();
+    var cur360 = props.globals.getNode('/sim/vr/cursors/mouse[0]/vr-mode');
+    var modeNode = props.globals.getNode('/devices/status/mice/mouse[0]/mode');
+    var mode = modeNode.getValue();
+    var msg = "";
+    # give correct feedback for the UI action which prompted this
+    var suffix = "Press Ctrl + TAB to ";
+    if (reason == "shift+right-click") {
+        suffix = "Shift + Right-click to ";
+    }
+    if (mode == 0) {
+        # Prevent accidental mouse capture without either VR cursor or VR running
+        var vrMouseAlways = props.globals.getNode('/sim/vr/cursors/mouse[0]/show-always').getBoolValue();
+        var vrRunning = props.globals.getNode('/sim/vr/running').getBoolValue();
+        if (vrMouseAlways or vrRunning) {
+            mode = 4;
+            msg = "360 mouse ON.";
+            suffix = suffix ~ "turn off.";
+            cur360.setBoolValue(true);
+        } else {
+            return;
+        }
+    } else if (mode == 4) {
+        mode = 0;
+        msg = "360 mouse OFF.";
+        suffix = suffix ~ "turn back on.";
+        cur360.setBoolValue(false);
+    } else {
+        return;
+    }
+
+    modeNode.setIntValue(mode);
+
+    msg = msg ~ " " ~ suffix;
+
+    fgcommand("show-message", props.Node.new({ "label": msg, "id":"mouse-mode" }));
+}
+
+addcommand("toggle-360-mouse", toggle360Mouse);
+
+var set360Mouse = func(enable)
+{
+    var cur360 = props.globals.getNode('/sim/vr/cursors/mouse[0]/vr-mode');
+    cur360.setBoolValue(enable);
+
+    var modeNode = props.globals.getNode('/devices/status/mice/mouse[0]/mode');
+    var mode = modeNode.getValue();
+    var msg = "";
+    if (mode == 0) {
+        if (!enable)
+            return;
+        mode = 4;
+        msg = "360 mouse ON.";
+    } else if (mode == 4) {
+        if (enable)
+            return;
+        mode = 0;
+        msg = "360 mouse OFF.";
+    } else {
+        return;
+    }
+
+    modeNode.setIntValue(mode);
+
+    # give correct feedback for the UI action which prompted this
+    if (enable) {
+        suffix = "Remove headset or press Ctrl + TAB to turn off.";
+    } else {
+        suffix = "Replace headset to turn back on.";
+    }
+    msg = msg ~ " " ~ suffix;
+
+    fgcommand("show-message", props.Node.new({ "label": msg, "id":"mouse-mode" }));
+}
+
+setlistener("/sim/vr/user-present", func(prop){
+    var trigger = props.globals.getNode('/sim/vr/cursors/mouse[0]/vr-mode-user-presence').getValue();
+    if (trigger) {
+        set360Mouse(prop.getValue());
+    }
+}, 0, 0);
 
 var setMouseFlightControlsSensitivity = func(sensitivity)
 {

defaults.xml
@@ -589,6 +589,8 @@ Started September 2000 by David Megginson, david@megginson.com
         <mouse n="0">
           <show-always type="bool" userarchive="y">false</show-always>
           <show-vr-running type="bool" userarchive="y">true</show-vr-running>
+          <vr-mode-user-presence type="bool" userarchive="y">true</vr-mode-user-presence>
+          <vr-mode type="bool">false</vr-mode>
         </mouse>
       </cursors>
       <depth-info type="bool" userarchive="y">true</depth-info>
@@ -596,6 +598,7 @@ Started September 2000 by David Megginson, david@megginson.com
       <mirror-enabled type="bool" userarchive="y">true</mirror-enabled>
       <mirror-mode type="string" userarchive="y">AUTOMATIC</mirror-mode>
       <mode type="string" userarchive="y">AUTOMATIC</mode>
+      <running type="bool">false</running>
       <swapchain-mode type="string" userarchive="y">AUTOMATIC</swapchain-mode>
       <validation-layer type="bool" userarchive="y">false</validation-layer>
       <visibility-mask type="bool" userarchive="y">true</visibility-mask>

gui/dialogs/vr-options.xml
@@ -207,6 +207,25 @@
     </checkbox>
   </group>
 
+  <group>
+    <layout>hbox</layout>
+    <halign>left</halign>
+    <default-padding>1</default-padding>
+    <text>
+      <label>  </label>
+    </text>
+    <checkbox>
+      <halign>left</halign>
+      <label>Enable 360 mouse when VR in use</label>
+      <property>/sim/vr/cursors/mouse[0]/vr-mode-user-presence</property>
+      <enable><property>/sim/vr/openxr/system/supports-user-presence</property></enable>
+      <live>true</live>
+      <binding>
+        <command>dialog-apply</command>
+      </binding>
+    </checkbox>
+  </group>
+
   <empty>
     <stretch>true</stretch>
   </empty>

keyboard.xml
@@ -123,17 +123,28 @@ top down before the key bindings are parsed.
 
  <!-- Tab key, also Ctrl-I, bit odd -->
  <key n="9">
-	<name>Tab</name>
-	<desc>Cycle mouse mode</desc>
-	<binding>
+  <name>Tab</name>
+  <desc>Cycle mouse mode</desc>
+  <binding>
 
-        <condition>
-            <not><property>/devices/status/keyboard/alt</property></not>
-            <not><property>/devices/status/keyboard/ctrl</property></not>
-        </condition>
-		<command>cycle-mouse-mode</command>
-		<reason>key-press</reason>
-	</binding>
+   <condition>
+    <not><property>/devices/status/keyboard/alt</property></not>
+    <not><property>/devices/status/keyboard/ctrl</property></not>
+   </condition>
+   <command>cycle-mouse-mode</command>
+   <reason>key-press</reason>
+  </binding>
+
+  <mod-ctrl>
+   <desc>Toggle 360 mouse</desc>
+   <binding>
+    <condition>
+     <not><property>/devices/status/keyboard/alt</property></not>
+    </condition>
+    <command>toggle-360-mouse</command>
+    <reason>key-press</reason>
+   </binding>
+  </mod-ctrl>
  </key>
 
  <key n="10"> <!-- OSG -->

mice.xml
@@ -15,7 +15,7 @@ current mode for each mouse is held in the
 
  <mouse n="0">
 
-  <mode-count type="int">4</mode-count>
+  <mode-count type="int">5</mode-count>
   <mode archive="y" type="int">0</mode>
 
 
@@ -52,6 +52,12 @@ current mode for each mouse is held in the
         <command>cycle-mouse-mode</command>
         <reason>right-click</reason>
     </binding>
+    <mod-shift>
+      <binding>
+        <command>toggle-360-mouse</command>
+        <reason>shift+right-click</reason>
+      </binding>
+    </mod-shift>
    </button>
 
    <!-- extra buttons control FoV -->
@@ -502,6 +508,54 @@ current mode for each mouse is held in the
     </x-axis-ctrl>
 
   </mode>
+
+  <!-- Mode 4: 360 mouse -->
+  <mode n="4">
+   <cursor>inherit</cursor>
+   <constrained type="bool">true</constrained>
+   <pass-through type="bool">false</pass-through>
+
+   <vr-cursor>
+    <condition>
+     <true/>
+    </condition>
+    <pass-through type="bool">true</pass-through>
+   </vr-cursor>
+
+   <!-- Mouse right/left motion -->
+   <x-axis>
+    <binding>
+     <command>property-assign</command>
+     <property>/sim/vr/cursors/mouse[0]/dx</property>
+     <property>/devices/status/mice/mouse/accel-x</property>
+    </binding>
+   </x-axis>
+
+   <!-- Mouse up/down motion -->
+   <y-axis>
+    <binding>
+     <command>property-assign</command>
+     <property>/sim/vr/cursors/mouse[0]/dy</property>
+     <property>/devices/status/mice/mouse/accel-y</property>
+    </binding>
+   </y-axis>
+
+   <button n="2">
+    <binding>
+     <command>cycle-mouse-mode</command>
+     <reason>right-click</reason>
+    </binding>
+
+    <mod-shift>
+      <binding>
+        <command>toggle-360-mouse</command>
+        <reason>shift+right-click</reason>
+      </binding>
+    </mod-shift>
+   </button>
+
+  </mode>
+
     
  </mouse> <!-- of mouse 0 -->
 

commit 262263185223253265a13774f14075bab983fde1

VR: Fix user presence listener when on from start
Set the VR user presence listener to trigger the callback immediately,
otherwise if the user is present from the start, the mouse won't
automatically switch to 360 mode, requiring manual switching or the user
to take off and put back on their headset in order to use the mouse.

Fixes: a4456cc65f06 ("VR: Enable 360 mouse mode")

Nasal/controls.nas
@@ -911,7 +911,7 @@ setlistener("/sim/vr/user-present", func(prop){
     if (trigger) {
         set360Mouse(prop.getValue());
     }
-}, 0, 0);
+}, 1, 0);
 
 var setMouseFlightControlsSensitivity = func(sensitivity)
 {